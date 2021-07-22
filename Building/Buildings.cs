365a7416bcfdc0f82c3fdd8be0945c8346d823c1
seancrossettie <seancrossettie@gmail.com>
2021-07-22 15:32:38 -0500

    Add project files.

---
+++ Building/Buildings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner.Buildings
+{
+    class Building
+    {
+        private string _designer = "Sean Rossettie";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume { 
+            get 
+            {
+                return Width * Depth * (3 * Stories);
+            } 
+        }
+
+        public Building(string address) =>_address = address;
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string name)
+        {
+            _owner = name;
+            Console.WriteLine($"{_address}------------");
+            Console.WriteLine($"Desgined by {_designer}");
+            Console.WriteLine($"Constructed on {_dateConstructed}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic meters of space");   
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UrbanPlanner.Buildings;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var TwentyOneEight = new Building("21 8th Ave North");
+            TwentyOneEight.Width = 100;
+            TwentyOneEight.Depth = 300;
+            TwentyOneEight.Stories = 4;
+
+            TwentyOneEight.Construct();
+            TwentyOneEight.Purchase("Matt");
+        }
+    }
+}
